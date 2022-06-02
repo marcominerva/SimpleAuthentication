a7c3eff09c816e2b8b70df7020088b4672f33cdd
Marco Minerva <marco.minerva@gmail.com>
2022-06-02 12:13:41 +0200

    Add other comments

--- src/SimpleAuthentication/ApiKey/ApiKeyValidationResult.cs
+++ src/SimpleAuthentication/ApiKey/ApiKeyValidationResult.cs
@@ -2,14 +2,29 @@
 
 namespace SimpleAuthentication.ApiKey;
 
+/// <summary>
+/// Contains the result of an API key authentication flow.
+/// </summary>
 public class ApiKeyValidationResult
 {
+    /// <summary>
+    /// <see langword="true"/> if authentication was successful; <see langword="false"/> otherwise.
+    /// </summary>
     public bool Succeeded { get; }
 
+    /// <summary>
+    /// Gets the authenticated username, if authentication was successful.
+    /// </summary>
     public string? Username { get; set; }
 
+    /// <summary>
+    /// Gets the claim list, if authentication was successful.
+    /// </summary>
     public IList<Claim>? Claims { get; }
 
+    /// <summary>
+    /// Gets the failure message, if authentication was unsuccessful.
+    /// </summary>
     public string? FailureMessage { get; }
 
     private ApiKeyValidationResult(string username, IList<Claim>? claims)
@@ -24,9 +39,22 @@ public class ApiKeyValidationResult
         FailureMessage = failureMessage;
     }
 
+    /// <summary>
+    /// Indicates that authentication was successful.
+    /// </summary>
+    /// <param name="username">The username to be used.</param>
+    /// <param name="claims">The claims list.</param>
+    /// <returns>The authentication result.</returns>
+    /// <seealso cref="IApiKeyValidator"/>
     public static ApiKeyValidationResult Success(string username, IList<Claim>? claims = null)
         => new(username, claims);
 
+    /// <summary>
+    /// Indicates that there was a failure during authentication.
+    /// </summary>
+    /// <param name="failureMessage">The failure message.</param>
+    /// <returns>The authentication result.</returns>
+    /// <seealso cref="IApiKeyValidator"/>
     public static ApiKeyValidationResult Fail(string failureMessage)
         => new(failureMessage);
 }
