c1df22a6902a9fc75d42e3bfaabdb0153392c245
Marco Minerva <marco.minerva@gmail.com>
2022-06-02 17:10:47 +0200

    Complete XML documentaion

--- src/SimpleAuthentication/JwtBearer/JwtBearerSettings.cs
+++ src/SimpleAuthentication/JwtBearer/JwtBearerSettings.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SimpleAuthentication.JwtBearer;
 
 /// <summary>
-/// Options class provides information needed to control Bearer Authentication handler behavior
+/// Options class provides information needed to control JWT Bearer Authentication handler behavior.
 /// </summary>
 public class JwtBearerSettings
 {
@@ -13,7 +15,7 @@ public class JwtBearerSettings
     public string SchemeName { get; set; } = JwtBearerDefaults.AuthenticationScheme;
 
     /// <summary>
-    /// Gets or sets the cryptographic algorithm that is used to generate the digital signature. (Default: HS256).
+    /// Gets or sets the cryptographic algorithm that is used to generate the digital signature (Default: HS256).
     /// </summary>
     /// <seealso cref="Microsoft.IdentityModel.Tokens.SecurityAlgorithms"/>
     public string Algorithm { get; set; } = "HS256";
@@ -23,13 +25,32 @@ public class JwtBearerSettings
     /// </summary>
     public string SecurityKey { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets the list that contains valid issuers that will be used to check against the token's issuer.
+    /// </summary>
+    /// <seealso cref="TokenValidationParameters.ValidIssuers"/>
     public string[]? Issuers { get; set; }
 
+    /// <summary>
+    /// Gets or sets the list that contains valid audiences that will be used to check against the token's audience.
+    /// </summary>
+    /// <seealso cref="TokenValidationParameters.ValidAudience"/>
     public string[]? Audiences { get; set; }
 
+    /// <summary>
+    /// Gets or sets the expiration time (relative to UTC current time) of the bearer token.
+    /// </summary>
     public TimeSpan? ExpirationTime { get; set; }
 
-    public TimeSpan ClockSkew { get; set; } = TimeSpan.FromMinutes(5);
+    /// <summary>
+    /// Gets or sets the clock skew to apply when validating a time (Default: 5 minutes).
+    /// </summary>
+    /// <seealso cref="TokenValidationParameters.ClockSkew"/>
+    public TimeSpan ClockSkew { get; set; } = TokenValidationParameters.DefaultClockSkew;
 
+    /// <summary>
+    /// <see langword="true"/> to register the <see cref="IJwtBearerService"/> service in the <see cref="IServiceCollection"/> (Default: true).
+    /// </summary>
+    /// <seealso cref="IJwtBearerService"/>
     public bool EnableJwtBearerService { get; set; } = true;
 }
